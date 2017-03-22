b393202c7ecee2f2c64997a7cb573cdc14aece37
Liang Yu <liangyu42087@gmail.com>
2017-03-22 22:39:25 +1100

    Added volley library for REST api.

--- app/src/main/java/deputy/android/com/deputyliang/MainActivity.java
+++ app/src/main/java/deputy/android/com/deputyliang/MainActivity.java
@@ -1,24 +1,41 @@
 package deputy.android.com.deputyliang;
 
 import android.content.Context;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Map;
+
 import deputy.android.com.deputyliang.adapter.ShiftAdapter;
 import deputy.android.com.deputyliang.model.Shift;
+import deputy.android.com.deputyliang.network.VolleyRequestQueue;
 import deputy.android.com.deputyliang.testing.ShiftTestUtil;
 
-public class MainActivity extends AppCompatActivity implements ShiftAdapter.ShiftAdapterOnClickHandler {
+public class MainActivity extends AppCompatActivity implements ShiftAdapter.ShiftAdapterOnClickHandler, Response.ErrorListener, Response.Listener, ImageLoader.ImageListener{
 
     private RecyclerView mRecyclerView;
     private ShiftAdapter mShiftAdapter;
     private TextView mEmptyMessageDisplay;
-
+    private String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +54,51 @@ public class MainActivity extends AppCompatActivity implements ShiftAdapter.Shif
         mRecyclerView.setAdapter(mShiftAdapter);
 
         mShiftAdapter.setShiftData(ShiftTestUtil.generateArrayOfFakeShift());
+        populateActionBar();
+    }
+
+    /*
+    Populate actionbar with value returned by https://apjoqdqpi3.execute-api.us-west-2.amazonaws.com/dmc
+     */
+    private void populateActionBar(){
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, VolleyRequestQueue.BUSINESS_URL, null, this, this){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                return VolleyRequestQueue.getHeaderParameter();
+            }
+        };
+        jsonObjectRequest.setTag(TAG);
+        VolleyRequestQueue.getInstance(this).addToRequestQueue(jsonObjectRequest);
+    }
+
+    @Override
+    public void onResponse(Object response) {
+        try {
+            if (response instanceof JSONObject) {
+                //Insert into DB.
+                JSONObject jsonObject = (JSONObject) response;
+                getSupportActionBar().setTitle(jsonObject.getString("name"));
+
+            } else if (response instanceof JSONObject[]) {
+
+            }
+        }catch (JSONException e){
+            Toast.makeText(this, getString(R.string.main_activity_volley_error), Toast.LENGTH_LONG).show();
+            Log.e(TAG, getString(R.string.main_activity_volley_error), e);
+        }
+    }
+
+    @Override
+    public void onResponse(ImageLoader.ImageContainer response, boolean isImmediate) {
+        if(response.getBitmap() != null){
+
+        }
+    }
+
+    @Override
+    public void onErrorResponse(VolleyError error) {
+        Toast.makeText(this, getString(R.string.main_activity_volley_error), Toast.LENGTH_LONG).show();
+        Log.e(TAG, getString(R.string.main_activity_volley_error), error);
     }
 
     @Override
@@ -54,5 +116,11 @@ public class MainActivity extends AppCompatActivity implements ShiftAdapter.Shif
         mRecyclerView.setVisibility(View.GONE);
         /* Then, show the error */
         mEmptyMessageDisplay.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        VolleyRequestQueue.getInstance(this).getRequestQueue().cancelAll(TAG);
     }
 }
---
+++ app/src/main/java/deputy/android/com/deputyliang/network/VolleyRequestQueue.java
@@ -0,0 +1,85 @@
+package deputy.android.com.deputyliang.network;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.LruCache;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import deputy.android.com.deputyliang.MainActivity;
+import deputy.android.com.deputyliang.model.Business;
+
+/**
+ * Created by liangyu42087 on 2017/3/22.
+ */
+
+public class VolleyRequestQueue {
+    public static final String BASE_URL = "https://apjoqdqpi3.execute-api.us-west-2.amazonaws.com/dmc";
+    public static final String BUSINESS_URL = BASE_URL + "/business";
+    public static final String PARAM_TOKEN = "Deputy 863407acb9e207441ec9e80d0debd4fe4489aa9b";
+    public static final String PARAM_AUTHORIZATION = "Authorization";
+
+    private static Map<String, String> PARAM_HEADER;
+
+    private static VolleyRequestQueue mInstance;
+    private RequestQueue mRequestQueue;
+    private static Context mContext;
+    private ImageLoader mImageLoader;
+    private JsonObjectRequest mJsonObjectRequest;
+
+    private VolleyRequestQueue(Context context) {
+        mContext = context;
+        mRequestQueue = getRequestQueue();
+        mImageLoader = new ImageLoader(mRequestQueue,
+                new ImageLoader.ImageCache() {
+                    private final LruCache<String, Bitmap>
+                            cache = new LruCache<String, Bitmap>(20);
+
+                    @Override
+                    public Bitmap getBitmap(String url) {
+                        return cache.get(url);
+                    }
+
+                    @Override
+                    public void putBitmap(String url, Bitmap bitmap) {
+                        cache.put(url, bitmap);
+                    }
+                });
+    }
+    public static synchronized VolleyRequestQueue getInstance(Context context) {
+        if (mInstance == null) {
+            mInstance = new VolleyRequestQueue(context);
+        }
+        return mInstance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            mRequestQueue = Volley.newRequestQueue(mContext.getApplicationContext());
+        }
+        return mRequestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        getRequestQueue().add(req);
+    }
+
+    public static Map<String, String> getHeaderParameter(){
+        if(PARAM_HEADER == null){
+            PARAM_HEADER = new HashMap<String, String>();
+            PARAM_HEADER.put(PARAM_AUTHORIZATION, PARAM_TOKEN);
+        }
+        return PARAM_HEADER;
+    }
+    public ImageLoader getImageLoader() {
+        return mImageLoader;
+    }
+}
